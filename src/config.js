5381e6561af5fc6b8e11c139de610882712876b3
lucky <takuto.yoshikai@gmail.com>
2021-09-17 12:20:17 +0900

    change rpc

--- src/config.js
+++ src/config.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === "development") {
 } else if (process.env.NODE_ENV === "production") {
   //TODO: Change here
   config = {
-    web3Host: "https://matic-mumbai.chainstacklabs.com",
+    web3Host: "https://rpc-mainnet.maticvigil.com",
     contractAddress: "0x1ACd77F5AAc3DC24d57F7270b2839B82eFAfc47C",
     gxApi: "https://asia-northeast1-gxcert-21233.cloudfunctions.net/gxcertMain",
     host: "https://gaiax.github.io/gxcert-app/#",
