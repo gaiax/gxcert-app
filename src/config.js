41e4e449aa8db90b529105ac3558e0b4ea328269
lucky <takuto.yoshikai@gmail.com>
2021-09-17 12:14:29 +0900

    change config

--- src/config.js
+++ src/config.js
@@ -3,17 +3,17 @@ let config;
 if (process.env.NODE_ENV === "development") {
   config = {
     web3Host: "https://matic-mumbai.chainstacklabs.com",
-    contractAddress: "0x38c009E363f7AcAEf5a29674192EF5edBe8cFE3f",
+    contractAddress: "0xE19F38e0fA7B005E8E62E837B0D79C8558fAd8E0",
     gxApi: "http://localhost:5001/gxcert-21233/asia-northeast1/gxcert",
-    host: "http://localhost:3000/gxcert-react2#",
+    host: "http://localhost:3000/#",
   };
 } else if (process.env.NODE_ENV === "production") {
   //TODO: Change here
   config = {
     web3Host: "https://matic-mumbai.chainstacklabs.com",
-    contractAddress: "0x38c009E363f7AcAEf5a29674192EF5edBe8cFE3f",
-    gxApi: "https://asia-northeast1-gxcert-21233.cloudfunctions.net/gxcert",
-    host: "https://gaiax.github.io/gxcert-react2/#",
+    contractAddress: "0x1ACd77F5AAc3DC24d57F7270b2839B82eFAfc47C",
+    gxApi: "https://asia-northeast1-gxcert-21233.cloudfunctions.net/gxcertMain",
+    host: "https://gaiax.github.io/gxcert-app/#",
   }
 }
 
