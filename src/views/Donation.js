3383336d5a1813ed429388e0bb80a89cbf0c436d
lucky <takuto.yoshikai@gmail.com>
2021-10-20 10:35:44 +0900

    tmp

--- src/views/Donation.js
+++ src/views/Donation.js
@@ -7,9 +7,10 @@ class Donation extends React.Component{
         <div className="donation-content">
           <p className="donation-title">寄付について</p>
           <div className="donation-description">
-            <p>このGxCertは、<b>皆様からの暗号通貨MATICの寄付をガス代として使用し、皆様の証明書を発行しています。</b></p>
-            <p>寄付金不足によって、もし証明書が発行できない場合、<b>MATICを寄付すると利用できます。</b></p>
-            <p>Gaiaxはこの寄付されたMATICを、GxCert運営以外の目的で移動、使用しません。</p>
+            <p>本サービスでは、暗号通貨MATICを利用し証明書の発行やサービスの維持をしています。サービス運営に必要なMATICは皆様からの寄付で賄われています。</p>
+            <p>MATICの不足により証明書が発行できない場合は、MATICを寄付することによって発行することができます。</p>
+            <p>また寄付されたMATICは本サービスの運営以外の目的で移動、使用は行いません。</p>
+            <p className="donation-address-title">寄付先アドレス</p>
             <p className="donation-address"><b className="donation-address-title">寄付先:</b> 0x21DAA5453e9d5BF9e33CC2406abBa1BC6338a22C</p>
             
           </div>
