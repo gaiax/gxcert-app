e300226abcd2194a55ce64b224d038891a57702d
lucky <takuto.yoshikai@gmail.com>
2021-10-20 11:28:37 +0900

    favicon and copy address

--- src/views/Donation.js
+++ src/views/Donation.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { copyToClipboard } from "../util/clipboard";
+import { FaCopy } from "react-icons/fa";
 
 class Donation extends React.Component{
   render() {
@@ -7,11 +9,13 @@ class Donation extends React.Component{
         <div className="donation-content">
           <p className="donation-title">寄付について</p>
           <div className="donation-description">
-            <p>本サービスでは、暗号通貨MATICを利用し証明書の発行やサービスの維持をしています。サービス運営に必要なMATICは皆様からの寄付で賄われています。</p>
-            <p>MATICの不足により証明書が発行できない場合は、MATICを寄付することによって発行することができます。</p>
+            <p>本サービスでは、暗号通貨MATICを利用し証明書の発行やサービスの維持をしています。<b>サービス運営に必要なMATICは皆様からの寄付で賄われています。</b></p>
+            <p>MATICの不足により証明書が発行できない場合は、<b>MATICを寄付することによって発行することができます。</b></p>
             <p>また寄付されたMATICは本サービスの運営以外の目的で移動、使用は行いません。</p>
             <p className="donation-address-title">寄付先アドレス</p>
-            <p className="donation-address"><b className="donation-address-title">寄付先:</b> 0x21DAA5453e9d5BF9e33CC2406abBa1BC6338a22C</p>
+            <div className="donation-address-container">
+              <p className="donation-address">0x21DAA5453e9d5BF9e33CC2406abBa1BC6338a22C</p> <span onClick={() => copyToClipboard("0x21DAA5453e9d5BF9e33CC2406abBa1BC6338a22C")}><FaCopy />コピー</span>
+            </div>
             
           </div>
         </div>
