a1772c0792aab75ec05930b860776376f46960eb
lucky <takuto.yoshikai@gmail.com>
2022-03-03 14:05:05 +0900

    br

--- src/views/SignIn.js
+++ src/views/SignIn.js
@@ -5,7 +5,8 @@ function SignIn(props) {
     <div className="sign-in">
       <div className="sign-in-content">
         <p className="sign-in-message">
-          サービスの利用にはGoogleアカウントでの認証が必須です。Googleアカウントで認証を行った後、登録を完了させてください。
+          サービスの利用にはGoogleアカウントでの認証が必須です。<br/>
+          Googleアカウントで認証を行った後、登録を完了させてください。
         </p>
         <img
           src={signInImage}
